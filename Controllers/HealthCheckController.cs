54e13d1b2fbff85d82e46a32b77991badfdbc7ad
Sunil-kumar <sunilku.951975@gmail.com>
2025-06-07 22:15:00 +0530

    first commit

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JerEntryWebApp.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Ok");
+        }
+    }
+}
---
+++ Models/JournalEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace JrEntryWebApi.Models
+{
+    public class JournalEntry
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
